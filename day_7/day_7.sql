8e5d82e9176a4ec3bad9f45ba3b2c99ae7f78403
Nikilehsa <nikilesha.nivas@gmail.com>
2025-06-12 22:30:31 +0530

    added day 7 files

---
+++ day_7/day_7.sql
@@ -0,0 +1,36 @@
+/*
+orders are stored in seperate tables (orders and ordersArchive)
+Combine all of them into one report without duplicates
+
+*/
+
+select 
+	    [OrderID]
+      ,[ProductID]
+      ,[CustomerID]
+      ,[SalesPersonID]
+      ,[OrderDate]
+      ,[ShipDate]
+      ,[OrderStatus]
+      ,[ShipAddress]
+      ,[BillAddress]
+      ,[Quantity]
+      ,[Sales]
+      ,[CreationTime]
+from sales.Orders
+union
+select 
+        [OrderID]
+      ,[ProductID]
+      ,[CustomerID]
+      ,[SalesPersonID]
+      ,[OrderDate]
+      ,[ShipDate]
+      ,[OrderStatus]
+      ,[ShipAddress]
+      ,[BillAddress]
+      ,[Quantity]
+      ,[Sales]
+      ,[CreationTime]
+from sales.OrdersArchive;
+
