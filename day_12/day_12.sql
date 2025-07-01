e99094482997a9b40eebd340b531f00c9a52bc72
Nikilehsa <nikilesha.nivas@gmail.com>
2025-07-01 11:20:43 +0530

    added day 12 sql file

---
+++ day_12/day_12.sql
@@ -0,0 +1,14 @@
+-- sort the customers from lowest to highes with null in the last
+
+select * 
+from sales.Customers
+order by score desc;
+
+-- find the sales price for each order by dividing the sales by the quantity
+
+select 
+	OrderID,
+	sales,
+	quantity,
+	sales/ nullif(Quantity,0) as yess
+from sales.Orders
