01b610cc5aeb85d20b09683ee9935b9d7719b833
Nikilehsa <nikilesha.nivas@gmail.com>
2025-07-02 12:40:38 +0530

    modified day 12 files

--- day_12/day_12.sql
+++ day_12/day_12.sql
@@ -1,14 +1,10 @@
 -- sort the customers from lowest to highes with null in the last
-
-select * 
+select *
 from sales.Customers
 order by score desc;
-
 -- find the sales price for each order by dividing the sales by the quantity
-
-select 
-	OrderID,
+select OrderID,
 	sales,
 	quantity,
-	sales/ nullif(Quantity,0) as yess
+	sales / nullif(Quantity, 0) as yess
 from sales.Orders
